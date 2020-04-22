8f8b50df23dad710d06cddd83362120b949e20c0
Bindu0612 <58510359+Bindu0612@users.noreply.github.com>
2020-04-22 17:52:31 +0530

    Travel ratecard(redux)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from 'reactstrap';
+import {connect} from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  render() {
+    return (
+
+      <Form className="form-style">
+        <FormGroup>
+          <div>
+            <Label className="labelClass" >Basefare </Label>
+          </div>
+
+          <div>
+            <Input id="Basefare" type="number" onChange = {this.props.baseFareValue} placeholder="Basefare" />
+          </div>
+
+        </FormGroup>
+
+        <FormGroup>
+          <div>
+            <Label className="labelClass"> GST </Label>
+          </div>
+          <div>
+            <Input type="number" value = {this.props.a.GST} id="GST" placeholder="Your GST..."
+            />
+          </div>
+        </FormGroup>
+
+        <Label> Total  :  </Label>
+        {this.props.a.total}
+        
+
+        <div style = {{textAlign :"center"}}>
+        <Button color="primary">Save</Button>
+        </div>
+      </Form>
+
+      
+
+
+    )
+  }
 }
 
-export default App;
+function mapStateToProps(state){
+  console.log(state);
+  return {
+    a:state
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return{
+    baseFareValue : (e) => dispatch({type:'BSFVALUE', data : e.target.value})
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (App);
---
+++ src/reducer.js
@@ -0,0 +1,17 @@
+function reducer(state = {basefare : "200", GST : null, total : null}, action){
+    console.log(action);
+var allFares = {...state};
+if (action.type === 'BSFVALUE'){
+    allFares.basefare = Number(action.data);
+    allFares.GST = allFares.basefare * 5 /100;
+    allFares.total = allFares.basefare + allFares.GST;
+    return allFares;
+}
+
+else{
+return state;
+}
+}
+
+
+export default reducer;
